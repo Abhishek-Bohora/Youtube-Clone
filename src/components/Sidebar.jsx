4be56329bca5e5d738669310afb7f48a6190756f
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-02-14 10:48:52 +0545

    sidebar layout done

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,42 @@
+import { categories } from "../utils/constants";
+import { Stack } from "@mui/material";
+
+const selectedCategory = "New";
+const Sidebar = () => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && "#FC1503",
+          color: "white",
+        }}
+        //we change the background color of the category which is selected
+        key={category.name}
+      >
+        <span
+          style={{
+            color: category.name === selectedCategory ? "white" : "red",
+            marginRight: "15px",
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
+        >
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default Sidebar;
